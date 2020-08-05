4518760780c96f8478bb0e3e73641447c69691f0
root <ansabm2015@gmail.com>
2020-08-05 19:53:14 +0530

    clone problem solved

--- static/files/js/evaluation-tableScroll.js
+++ static/files/js/evaluation-tableScroll.js
@@ -1,13 +1,13 @@
 // requires jquery library
-jQuery(document).ready(function () {
-  jQuery(".eval-table").clone(true).appendTo('#table-scroll').addClass('clone');
-});
+// jQuery(document).ready(function () {
+//   jQuery(".eval-table").clone(true).appendTo('#table-scroll').addClass('clone');
+// });
 //# sourceURL=pen.js
  
  
  
  // requires jquery library
-jQuery(document).ready(function () {
-  jQuery(".resource-table").clone(true).appendTo('#resource-scroll').addClass('res-clone');
-});
+// jQuery(document).ready(function () {
+//   jQuery(".resource-table").clone(true).appendTo('#resource-scroll').addClass('res-clone');
+// });
 //# sourceURL=pen.js
