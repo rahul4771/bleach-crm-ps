44c46dcd27222c87fc5023a6d735d85f025e519c
root <ansabm2015@gmail.com>
2020-10-28 15:42:11 +0530

    login redirection added

--- customer/templatetags/customtags.py
+++
@@ -1,8 +0,0 @@
-from num2words import num2words
-from django import template
-register = template.Library()
-
-@register.simple_tag()
-def num_to_words(digit, *args, **kwargs):
-    digit_to_words = num2words(digit).upper()
-    return digit_to_words
