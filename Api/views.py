f76f112f4908974290197f648a84000c319132d5
root <ansabm2015@gmail.com>
2020-11-25 18:42:41 +0530

    changes

--- Api/views.py
+++ Api/views.py
@@ -123,7 +123,14 @@ class PaymentResponseCredit(APIView):
 
 		payment_result = request.POST.get('decision')
 
+		print(request.POST)
+		print(evaluation_id)
+		print(payment_mode)
+		print(amount)
+		print(order)
+
 		if order and payment_result == 'ACCEPT':
+			print("success")
 			#Receipt Number
 			receipt_no               = PaymentHistory.objects.filter(is_active=True,receipt_no__isnull=False).aggregate(t=Max('receipt_no'))['t'] or int(str(timezone.now().year)[-2:]+str(timezone.now().month).zfill(2)+'10000')
 			current_receipt_starting = int(str(timezone.now().year)[-2:]+str(timezone.now().month).zfill(2))
