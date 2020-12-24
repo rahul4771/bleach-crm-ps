4dcf566436f0bdec223428ccf99ab28e882ea6f3
root <ansabm2015@gmail.com>
2020-12-24 19:00:27 +0530

    changes

--- bleachadmin/views.py
+++ bleachadmin/views.py
@@ -692,13 +692,8 @@ class FeedbackDetails(IsAdmin,View):
 
 
 		#to find starring caluculations in whole system
-		try:
-			feedbacks                 = FeedBack.objects.filter(is_active=True)
-		except:
-			feedbacks				  = None
-
-		total_feedbacks               = feedbacks.values('order').distinct().count()
 		full_order_wise_feedbacks     = order_wise_feedbacks
+		total_feedbacks               = order_wise_feedbacks.filter(is_feedback_marked=True).count()
 				
 				
 		#PAGINATION FEEDBACKS		
@@ -727,7 +722,7 @@ class FeedbackDetails(IsAdmin,View):
 		page_range = list(paginator.page_range)[start_index:end_index]	
 		entry_per_page=(order_wise_feedbacks.end_index())-(order_wise_feedbacks.start_index())+1
 
-		return render(request,'admin/feedback/feedbacks.html',{"feedbacks":feedbacks,"total_feedbacks":total_feedbacks,"order_wise_feedbacks":order_wise_feedbacks,"full_order_wise_feedbacks":full_order_wise_feedbacks,"search_query":search,"page_range":page_range,"entry_per_page":entry_per_page,"no_of_entries":no_of_entries,"governorates":governorates,"areas":areas,"service_types":service_types,"fil_governorate":fil_governorate,"fil_area":fil_area,"fil_minimumstarring":fil_minimumstarring,"fil_maximumstarring":fil_maximumstarring,"fil_service_type":fil_service_type,})
+		return render(request,'admin/feedback/feedbacks.html',{"total_feedbacks":total_feedbacks,"order_wise_feedbacks":order_wise_feedbacks,"full_order_wise_feedbacks":full_order_wise_feedbacks,"search_query":search,"page_range":page_range,"entry_per_page":entry_per_page,"no_of_entries":no_of_entries,"governorates":governorates,"areas":areas,"service_types":service_types,"fil_governorate":fil_governorate,"fil_area":fil_area,"fil_minimumstarring":fil_minimumstarring,"fil_maximumstarring":fil_maximumstarring,"fil_service_type":fil_service_type,})
 
 class FeedbackAdvanced(IsAdmin,View):
 	def get(self,request,client_id,order_id):
