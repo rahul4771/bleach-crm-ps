96cbf169eea28bf5303b6f21deb3dcc1bc7d5748
ADHILSHA <aadilshah56@gmail.com>
2021-03-04 14:38:03 +0530

    oil residue name changed

--- static/files/externalfiles/js/formClone.js
+++ static/files/externalfiles/js/formClone.js
@@ -1,9 +1,10 @@
 var counter = 1;
 
-
 /* Clone Form*/
  function addItem(){
-        
+    var service=$( "#bk-service option:selected" ).text();
+
+     var residueVal=$("input[name='bk-oil_residue-1']:checked").val();
       var checkVal=$("input[name='bk-stain-1']:checked").val();
         var el = $('#bk-item-1').clone().attr({'id': 'bk-item-' + ++counter}).appendTo('#bk-form');
        
@@ -18,10 +19,25 @@ var counter = 1;
         $('#bk-item-'+counter).find('#bk-age-1').attr({'id': 'bk-age-'+counter,'name': 'bk-age-'+counter}).val(''); /*To change name & id of age*/
         $('#bk-item-'+counter).find("#bk-material-1").attr({'id': 'bk-material-'+counter,'name': 'bk-material-'+counter}).val(''); /*To change name of material*/
         $('#bk-item-'+counter).find("#bk-color-1").attr({'id': 'bk-color-'+counter,'name': 'bk-color-'+counter}).val(''); /*To change name of color*/
-        $('#bk-item-'+counter).find('#bk-stain-1-1').attr({'id': 'bk-stain-'+counter+'-1','name': 'bk-stain-'+counter,'value':'yes'}); /*To change name & id of stain (yes)*/
+        if(service=='Kitchen Cleaning'){
+            $('#bk-item-'+counter).find('#bk-stain-1-1').attr({'id': 'bk-stain-'+counter+'-1','name': 'bk-oil_residue-'+counter,'value':'true'}); /*To change name & id of oil residue (true)*/
+            $('#bk-item-'+counter).find('#bk-stain-1-2').attr({'id': 'bk-stain-'+counter+'-2','name': 'bk-oil_residue-'+counter,'value':'false'}); /*To change name & id of oil residue (false) */
+
+        }
+        else{
+            $('#bk-item-'+counter).find('#bk-stain-1-1').attr({'id': 'bk-stain-'+counter+'-1','name': 'bk-stain-'+counter,'value':'yes'}); /*To change name & id of stain (yes)*/
         $('#bk-item-'+counter).find('#bk-stain-1-2').attr({'id': 'bk-stain-'+counter+'-2','name': 'bk-stain-'+counter,'value':'no'}); /*To change name & id of stain (no) */
+        }
+        
        // $("input[name='bk-stain-"+counter+"']:checked").val('no');
+       if($("input[name='bk-stain-"+counter+"']").length){
         $("input[name='bk-stain-"+counter+"']")[1].checked=true;
+       }
+       if($("input[name='bk-oil_residue-"+counter+"']").length){
+           console.log("i m here");
+        $("input[name='bk-oil_residue-"+counter+"']")[1].checked=true;
+       }
+       
      
         $('#bk-item-'+counter).find("#bk-stain-reason-1").attr({'id': 'bk-stain-reason-'+counter,'name': 'bk-stain-reason-'+counter}); /*To change name of reason for stain*/
        
@@ -39,6 +55,7 @@ var counter = 1;
         $('#bk-del-btn-'+counter).show()
 
         $('#sectioncounter_id').val(counter);
+        console.log("residue val is "+residueVal);
         if(checkVal=='yes'){
             $("input[name='bk-stain-1']:checked").val('yes');
             $("input[name='bk-stain-1']")[0].checked=true;
@@ -47,8 +64,25 @@ var counter = 1;
              
          }
          else{
-            $("input[name='bk-stain-1']")[0].checked=false;
-            $("input[name='bk-stain-1']")[1].checked=true;
+            if(checkVal=='no'){
+                $("input[name='bk-stain-1']")[0].checked=false;
+                $("input[name='bk-stain-1']")[1].checked=true;
+            }
+           
+         }
+         if(residueVal=='true'){
+            $("input[name='bk-oil_residue-1']:checked").val('true');
+            $("input[name='bk-oil_residue-1']")[0].checked=true;
+            $("input[name='bk-oil_residue-1']")[1].checked=false;
+           
+             
+         }
+         else{
+            if(residueVal=='false'){
+                $("input[name='bk-oil_residue-1']")[0].checked=false;
+                $("input[name='bk-oil_residue-1']")[1].checked=true;
+            }
+           
          }
  }
 
@@ -57,7 +91,8 @@ var counter = 1;
 
  function removeDiv(elem){
       /* To find Children */
-    
+      var service=$( "#bk-service option:selected" ).text();
+
     var childDiv = [];
     $("#bk-form > div").each((index, elem) => {
         childDiv.push(elem.id);
@@ -81,9 +116,16 @@ var counter = 1;
     $('#bk-age-'+nextVal).attr({'id': 'bk-age-'+currentVal,'name': 'bk-age-'+currentVal});
     $('#bk-material-'+nextVal).attr({'id': 'bk-material-'+currentVal,'name': 'bk-material-'+currentVal});
     $('#bk-color-'+nextVal).attr({'id': 'bk-color-'+currentVal,'name': 'bk-color-'+currentVal});
-    $('#bk-stain-'+nextVal+'-1').attr({'id': 'bk-stain-'+currentVal+'-1','name': 'bk-stain-'+currentVal});
-    $('#bk-stain-'+nextVal+'-2').attr({'id': 'bk-stain-'+currentVal+'-2','name': 'bk-stain-'+currentVal});
-    $('#bk-stain-reason-'+nextVal).attr({'id': 'bk-stain-reason-'+currentVal,'name': 'bk-stain-reason-'+currentVal});
+    if(service=="Kitchen Cleaning"){
+        $('#bk-stain-'+nextVal+'-1').attr({'id': 'bk-stain-'+currentVal+'-1','name': 'bk-oil_residue-'+currentVal});
+        $('#bk-stain-'+nextVal+'-2').attr({'id': 'bk-stain-'+currentVal+'-2','name': 'bk-oil_residue-'+currentVal});
+    }
+    else{
+        $('#bk-stain-'+nextVal+'-1').attr({'id': 'bk-stain-'+currentVal+'-1','name': 'bk-stain-'+currentVal});
+        $('#bk-stain-'+nextVal+'-2').attr({'id': 'bk-stain-'+currentVal+'-2','name': 'bk-stain-'+currentVal});
+        $('#bk-stain-reason-'+nextVal).attr({'id': 'bk-stain-reason-'+currentVal,'name': 'bk-stain-reason-'+currentVal});
+    }
+   
 
    }
    
@@ -106,9 +148,16 @@ var counter = 1;
                 $('#bk-age-'+itemId).attr({'id': 'bk-age-'+(parseInt(itemId)-1),'name': 'bk-age-'+(parseInt(itemId)-1)});
                 $('#bk-material-'+itemId).attr({'id': 'bk-material-'+(parseInt(itemId)-1),'name': 'bk-material-'+(parseInt(itemId)-1)});
                 $('#bk-color-'+itemId).attr({'id': 'bk-color-'+(parseInt(itemId)-1),'name': 'bk-color-'+(parseInt(itemId)-1)});
-                $('#bk-stain-'+itemId+'-1').attr({'id': 'bk-stain-'+(parseInt(itemId)-1)+'-1','name': 'bk-stain-'+(parseInt(itemId)-1)});
+                if(service=="Kitchen Cleaning"){
+                    $('#bk-stain-'+itemId+'-1').attr({'id': 'bk-stain-'+(parseInt(itemId)-1)+'-1','name': 'bk-oil_residue-'+(parseInt(itemId)-1)});
+                    $('#bk-stain-'+itemId+'-2').attr({'id': 'bk-stain-'+(parseInt(itemId)-1)+'-2','name': 'bk-oil_residue-'+(parseInt(itemId)-1)});
+                }
+                else{
+                    $('#bk-stain-'+itemId+'-1').attr({'id': 'bk-stain-'+(parseInt(itemId)-1)+'-1','name': 'bk-stain-'+(parseInt(itemId)-1)});
                 $('#bk-stain-'+itemId+'-2').attr({'id': 'bk-stain-'+(parseInt(itemId)-1)+'-2','name': 'bk-stain-'+(parseInt(itemId)-1)});
                 $('#bk-stain-reason-'+itemId).attr({'id': 'bk-stain-reason-'+(parseInt(itemId)-1),'name': 'bk-stain-reason-'+(parseInt(itemId)-1)});
+                }
+                
 
             }          
         }
