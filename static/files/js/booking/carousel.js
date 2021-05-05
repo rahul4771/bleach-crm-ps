13fe77b74b1a847617b9a2406df0bfdcc244052c
ADHILSHA <aadilshah56@gmail.com>
2021-05-05 18:56:49 +0530

    booking page initiated

---
+++ static/files/js/booking/carousel.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+$('#category-carousel').owlCarousel({
+    loop:false,
+    
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+$('#service-carousel').owlCarousel({
+    loop:false,
+    
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+})
