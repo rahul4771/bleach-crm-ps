9b109589bc78ea1551bd58c350a21a867b51db69
root <ansab.m@bleach-kw.com>
2021-05-14 14:45:40 +0530

    changes

--- customer/admin.py
+++ customer/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from customer.models import CustomerBooking
+from customer.models import CustomerBooking,NewCustomerOtp
 
 # Register your models here.
 admin.site.register(CustomerBooking)
+admin.site.register(NewCustomerOtp)
