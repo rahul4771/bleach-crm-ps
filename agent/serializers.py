648e3435f6da8116e155ecd6d44b1ff234419e98
root <ansab.m@bleach-kw.com>
2021-05-19 16:59:21 +0530

    changes

--- agent/serializers.py
+++ agent/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from customer.serilizers import AddressSerializer
-from order.models import OrderScheduler,FollowUpScheduler,FollowUp
+from order.models import Order,OrderScheduler,FollowUpScheduler,FollowUp
 from evaluator.models import EvaluationDetails
 from user.models import UserProfile
 
@@ -11,19 +11,26 @@ class UserProfileShowSerializer(serializers.ModelSerializer):
 	class Meta:
 		model  = UserProfile
 		fields = ('id','name','gender','email','mobile_number','profile_image')	
-		read_only_fields =('id',)
+
+
+class OrderShowSerializer(serializers.ModelSerializer):
+	class Meta:
+		model  = Order
+		fields = ('order_no','order_status','payment_status')
 
 class EvaluationDetailsShowSerializer(serializers.ModelSerializer): 
-	evaluator                          = UserProfileShowSerializer(read_only=True)
+	evaluator          = UserProfileShowSerializer(read_only=True) 
 	class Meta:
 		model  = EvaluationDetails
-		fields = ('evaluator','evaluation_id')
+		fields = ('evaluator',)
+
 
 class CleaningScheduleSerializer(serializers.ModelSerializer):
 	start_at           = serializers.DateTimeField(format="%d-%m-%Y %I:%M %p")
 	end_at             = serializers.DateTimeField(format="%d-%m-%Y %I:%M %p")
 	customer_address   = AddressSerializer(read_only=True)
 	evaluation_details = EvaluationDetailsShowSerializer(read_only=True)
+	order              = OrderShowSerializer(read_only=True)
 	class Meta:
 		model  = OrderScheduler
 		fields = ('id','start_at','end_at','customer_address','work_status','no_of_cleaners','cleaning_hours','evaluation_details')
