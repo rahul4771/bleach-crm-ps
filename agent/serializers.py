c15482e173ff7d79bf10bf038a343a282bf69d77
root <ansab.m@bleach-kw.com>
2021-05-20 17:34:44 +0530

    evaluation book and followup

--- agent/serializers.py
+++ agent/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from customer.serilizers import AddressSerializer
 from order.models import Order,OrderScheduler,FollowUpScheduler,FollowUp
-from evaluator.models import EvaluationDetails
+from evaluator.models import EvaluationDetails,EvaluationBook
 from user.models import UserProfile
 from senior_team_leader.models import CleaningTeam,FollowUpTeam
 
@@ -24,6 +24,11 @@ class EvaluationDetailsShowSerializer(serializers.ModelSerializer):
 		model  = EvaluationDetails
 		fields = ('evaluator',)
 
+class EvaluationBookShowSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = EvaluationBook
+		fields = ('cleaning_policy',)
+
 class CleaningTeamShowSerializer(serializers.ModelSerializer):
 	team_leader = UserProfileShowSerializer(read_only=True)
 	class Meta:
@@ -32,15 +37,16 @@ class CleaningTeamShowSerializer(serializers.ModelSerializer):
 
 
 class CleaningScheduleSerializer(serializers.ModelSerializer):
-	start_at           = serializers.DateTimeField(format="%d-%m-%Y %I:%M %p")
-	end_at             = serializers.DateTimeField(format="%d-%m-%Y %I:%M %p")
-	customer_address   = AddressSerializer(read_only=True)
-	evaluation_details = EvaluationDetailsShowSerializer(read_only=True)
-	order              = OrderShowSerializer(read_only=True)
+	start_at             = serializers.DateTimeField(format="%d-%m-%Y %I:%M %p")
+	end_at               = serializers.DateTimeField(format="%d-%m-%Y %I:%M %p")
+	customer_address     = AddressSerializer(read_only=True)
+	evaluation_details   = EvaluationDetailsShowSerializer(read_only=True)
+	order                = OrderShowSerializer(read_only=True)
+	order_scheduler_book = EvaluationBookShowSerializer(read_only=True)
 	cleaning_team_order_scheduler = CleaningTeamShowSerializer(many=True,read_only=True)
 	class Meta:
 		model  = OrderScheduler
-		fields = ('id','start_at','end_at','customer_address','work_status','no_of_cleaners','cleaning_hours','evaluation_details','order','cleaning_team_order_scheduler')
+		fields = ('id','start_at','end_at','customer_address','work_status','no_of_cleaners','cleaning_hours','evaluation_details','order','order_scheduler_book','cleaning_team_order_scheduler')
 
 
 
