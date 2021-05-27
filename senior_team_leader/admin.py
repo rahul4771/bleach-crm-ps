d97d07c66250f13430b39ab43c24245fdaf84e86
root <ansab.m@bleach-kw.com>
2021-05-27 13:50:54 +0530

    search option added

--- senior_team_leader/admin.py
+++ senior_team_leader/admin.py
@@ -5,19 +5,26 @@ from senior_team_leader.models import CleaningTeam,CleaningTeamMedia,CleaningTea
 class CleaningTeamMediaInline(admin.TabularInline):
 	model   = CleaningTeamMedia
 	extra	= 1
-
 class CleaningTeamAdmin(admin.ModelAdmin):
-	inlines = (CleaningTeamMediaInline,)
+	inlines       = (CleaningTeamMediaInline,)
+	search_fields = ['order_scheduler__order__order_no']
+admin.site.register(CleaningTeam,CleaningTeamAdmin)
+
 
 class CleaningTeamMediaAdmin(admin.ModelAdmin):
 	radio_fields = {"media_type":admin.HORIZONTAL,"taken_status":admin.HORIZONTAL}
-
-admin.site.register(CleaningTeam,CleaningTeamAdmin)
 admin.site.register(CleaningTeamMedia,CleaningTeamMediaAdmin)
 
 
+class CleaningTeamMemberAdmin(admin.ModelAdmin):
+	search_fields = ['team__order_scheduler__order__order_no']
+admin.site.register(CleaningTeamMember,CleaningTeamMemberAdmin)
+
+
+
 admin.site.register(CleaningTeamTask)
-admin.site.register(CleaningTeamMember)
+
+
 
 class FollowUpTeamMediaInline(admin.TabularInline):
 	model   = FollowUpTeamMedia
