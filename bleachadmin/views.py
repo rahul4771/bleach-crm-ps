cad3094091c1ebff2d206af03721e3680a8c644c
root <ansab.m@bleach-kw.com>
2021-06-04 17:06:54 +0530

    changes

--- bleachadmin/views.py
+++ bleachadmin/views.py
@@ -40,6 +40,32 @@ from django.template.loader import render_to_string
 class AdminHome(IsAdmin,View):
 	def get(self,request):
 
+		#cleaners and leaders
+		cleaners = UserProfile.objects.filter(is_active=True,user_type='CLEANER')
+		leaders  = UserProfile.objects.filter(is_active=True,user_type='LEADER')
+		count    = 0
+		
+		for cleaner in cleaners:
+			if count%2 == 0:
+				cleaner.shift_start = datetime.strptime('06:00 AM','%I:%M %p')
+				cleaner.shift_end   = datetime.strptime('06:00 PM','%I:%M %p')
+			else:
+				cleaner.shift_start = datetime.strptime('09:00 AM','%I:%M %p')
+				cleaner.shift_end   = datetime.strptime('09:00 PM','%I:%M %p')
+			count = count+1
+			cleaner.save()
+
+		for leader in leaders:
+			if count%2 == 0:
+				leader.shift_start  = datetime.strptime('06:00 AM','%I:%M %p')
+				leader.shift_end    = datetime.strptime('06:00 PM','%I:%M %p')
+			else:
+				leader.shift_start  = datetime.strptime('09:00 AM','%I:%M %p')
+				leader.shift_end    = datetime.strptime('09:00 PM','%I:%M %p')
+			count = count+1
+			leader.save()
+
+
 		#evaluators
 		evaluators_sales_target = UserProfile.objects.filter(is_active=True,user_type='EVALUATOR')
 		#for taking today counts
