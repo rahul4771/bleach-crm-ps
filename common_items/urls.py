4eb40d04904aec37608207be495a33530f1391a9
Rangeen <rangeens582@gmail.com>
2021-06-07 14:24:11 +0530

    resourcenew

--- common_items/urls.py
+++ common_items/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
 		url(r'^daily-sales/$',views.DailySales.as_view(),name='daily-sales'),
 		url(r'^makequatation/phase1/edit/(?P<enquiry_id>[-\w]+)/(?P<evaluation_id>[-\w]+)/$',views.MakeQuatationPhase1Edit.as_view(),name='makequatation1edit'),
 		url(r'^resources/$',views.ResourceManagement.as_view(),name='resource-management'),
+
+		url(r'^resources-new/$',views.ResourceManagementTest.as_view(),name='resource-management-new'),
 	
 		url(r'^feedbacks/$',views.FeedbackDetails.as_view(),name='feedbacks'),
 		url(r'^feedback/details/(?P<client_id>[-\w]+)/(?P<order_id>[-\w]+)/$',views.FeedbackAdvanced.as_view(),name='feedbackadvanced'),
