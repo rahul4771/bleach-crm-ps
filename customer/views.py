ac30030a21e29d10235e66a30af4ceab7f1a83ef
Ansab M <ansab.m@bleach-kw.com>
2021-06-14 20:04:34 +0530

    invoice

--- customer/views.py
+++ customer/views.py
@@ -833,6 +833,7 @@ def testinvoice_html_to_pdf_view(request,evaluation_id):
 	order = Order.objects.select_related('evaluation__customer').prefetch_related(Prefetch('order_scheduler_order',queryset=OrderScheduler.objects.filter(is_active=True).select_related('evaluation_details','order_scheduler_book','customer_address__area','customer_address__governorate').prefetch_related(Prefetch('order_scheduler_book__evaluationsection_book',queryset=EvaluationBookSection.objects.filter(is_active=True).prefetch_related(Prefetch('keynotesections',queryset=EvaluationSectionKeynote.objects.filter(is_active=True),to_attr='sectionkeynotes')),to_attr='evaluationbooksection')),to_attr='orderschedules')).get(is_active=True,evaluation__evaluation_id=evaluation_id,evaluation__customer__username=user_name)
 
 	nonduplicate_schedules = []
+	
 	#Remove duplicates for subscription
 	duplicate_schedules    = []
 	for orderschedule in order.orderschedules:
@@ -843,23 +844,16 @@ def testinvoice_html_to_pdf_view(request,evaluation_id):
 
 		duplicate_schedules.append(orderschedule.order_scheduler_book)
 
-	#for credit card
-	full_name_array = UserProfile.objects.get(username=user_name).name.split()
-	firstname = full_name_array[0]
-	lastname  = ''
-	
-	count = 0
-	for i in full_name_array:
-		if(count>=1):
-			lastname += i+' '
-		count += 1
-	
-	customer_ip_address = get_client_ip(request)
+	#per completed jobs count
+	completed_jobs_count = 0 
+	for schedule in order.orderschedules:
+		if schedule.work_status == 'CLEANING_FULFILLED':
+			completed_jobs_count += 1
 
 	# return render(request,"customer/customer_invoice.html",{'order':order,'nonduplicate_schedules':nonduplicate_schedules,'firstname':firstname,'lastname':lastname,'customer_ip_address':customer_ip_address,})
     
 
-	html_string = render_to_string("customer/downloads/invoice.html",{'order':order,'nonduplicate_schedules':nonduplicate_schedules,'firstname':firstname,'lastname':lastname,'customer_ip_address':customer_ip_address,})
+	html_string = render_to_string("customer/downloads/invoice.html",{'order':order,'nonduplicate_schedules':nonduplicate_schedules,'completed_jobs_count':completed_jobs_count})
 
 	html = HTML(string=html_string,base_url=request.build_absolute_uri())
 	html.write_pdf(target='/home/sonu/pdf/tmp/invoice/invoice.pdf');
