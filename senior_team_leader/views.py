2130c007021b3bec7111a7a6380b1532d7fda3f4
Ansab M <ansab.m@bleach-kw.com>
2021-06-23 19:14:34 +0530

    time

--- senior_team_leader/views.py
+++ senior_team_leader/views.py
@@ -1084,8 +1084,8 @@ class AssigncleaningTeam(IsSeniorTeamLeader,View):
 
 		#shceduled order details
 		order_schedule    = OrderScheduler.objects.select_related('evaluation_details__evaluation','order_scheduler_book__service_type').prefetch_related(Prefetch('order_scheduler_book__evaluationbookmedia',queryset=EvaluationMedia.objects.filter(is_active=True),to_attr="evaluationmedias"),Prefetch('order_scheduler_book__evaluationsection_book',queryset=EvaluationBookSection.objects.filter(is_active=True).prefetch_related(Prefetch('keynotesections',queryset=EvaluationSectionKeynote.objects.filter(is_active=True),to_attr='keynotes')),to_attr='sections')).get(is_active=True,id=scheduler_id)
-		start_at_datetime = order_schedule.start_at+timedelta(hours=3)
-		end_at_datetime   = order_schedule.end_at+timedelta(hours=3)
+		start_at_datetime = order_schedule.start_at
+		end_at_datetime   = order_schedule.end_at
 		start_at_date     = start_at_datetime.date()
 		end_at_date       = end_at_datetime.date()
 		start_at_time     = start_at_datetime.time()
@@ -1124,7 +1124,7 @@ class AssigncleaningTeam(IsSeniorTeamLeader,View):
 		end_at_time       = end_at_datetime.time()
 
 		#same time schedules
-		order_schedules = OrderScheduler.objects.filter(start_at=order_schedule.start_at,end_at=order_schedule.end_at,evaluation_details__evaluation=order_schedule.evaluation_details.evaluation).select_related('evaluation_details__evaluation','order_scheduler_book__service_type').prefetch_related(Prefetch('order_scheduler_book__evaluationbookmedia',queryset=EvaluationMedia.objects.filter(is_active=True),to_attr="evaluationmedias"),Prefetch('order_scheduler_book__evaluationsection_book',queryset=EvaluationBookSection.objects.filter(is_active=True).prefetch_related(Prefetch('keynotesections',queryset=EvaluationSectionKeynote.objects.filter(is_active=True),to_attr='keynotes')),to_attr='sections'))	
+		order_schedules = OrderScheduler.objects.filter(start_at=start_at_datetime,end_at=end_at_datetime,evaluation_details__evaluation=order_schedule.evaluation_details.evaluation).select_related('evaluation_details__evaluation','order_scheduler_book__service_type').prefetch_related(Prefetch('order_scheduler_book__evaluationbookmedia',queryset=EvaluationMedia.objects.filter(is_active=True),to_attr="evaluationmedias"),Prefetch('order_scheduler_book__evaluationsection_book',queryset=EvaluationBookSection.objects.filter(is_active=True).prefetch_related(Prefetch('keynotesections',queryset=EvaluationSectionKeynote.objects.filter(is_active=True),to_attr='keynotes')),to_attr='sections'))	
 
 		#to block back button submission
 		if order_schedule.work_status=='CLEANING_TEAM_ASSIGNED':
@@ -1148,8 +1148,8 @@ class AssigncleaningTeam(IsSeniorTeamLeader,View):
 			if	cleaning_team_assign_form.is_valid() and check_cleaners.count() >= len(assigned_cleaners) and check_tl:
 				cleaning_team_assign_form_save                   = cleaning_team_assign_form.save(commit=False)
 				cleaning_team_assign_form_save.order_scheduler   = order_schedule
-				cleaning_team_assign_form_save.start_at          = order_schedule.start_at
-				cleaning_team_assign_form_save.end_at            = order_schedule.end_at
+				cleaning_team_assign_form_save.start_at          = start_at_datetime
+				cleaning_team_assign_form_save.end_at            = end_at_datetime
 				cleaning_team_assign_form_save.created_by        = request.user
 				cleaning_team_assign_form_save.save()
 
@@ -1161,8 +1161,8 @@ class AssigncleaningTeam(IsSeniorTeamLeader,View):
 				#cleaners
 				assigned_cleaners_list   = []
 				for cleaner in assigned_cleaners:
-					assigned_cleaners_list.append(CleaningTeamMember(team=cleaning_team_assign_form_save,member_id=cleaner,start_at=order_schedule.start_at,end_at=order_schedule.end_at,start_time=order_schedule.start_at.time(),end_time=order_schedule.end_at.time()))
-				assigned_cleaners_list.append(CleaningTeamMember(team=cleaning_team_assign_form_save,member=cleaning_team_assign_form_save.team_leader,start_at=order_schedule.start_at,end_at=order_schedule.end_at,start_time=order_schedule.start_at.time(),end_time=order_schedule.end_at.time()))
+					assigned_cleaners_list.append(CleaningTeamMember(team=cleaning_team_assign_form_save,member_id=cleaner,start_at=start_at_datetime.start_at,end_at=end_at_datetime,start_time=start_at_time,end_time=end_at_time))
+				assigned_cleaners_list.append(CleaningTeamMember(team=cleaning_team_assign_form_save,member=cleaning_team_assign_form_save.team_leader,start_at=start_at_datetime,end_at=end_at_datetime,start_time=start_at_time,end_time=end_at_time))
 				#bulk create
 				CleaningTeamMember.objects.bulk_create(assigned_cleaners_list)	
 
