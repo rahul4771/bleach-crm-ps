d410efcdba3ef219e4c028c081b5d8d362a44c07
Rangeen <rangeens582@gmail.com>
2021-07-01 10:43:00 +0530

    urls

--- static/files/js/calendar/main.js
+++ static/files/js/calendar/main.js
@@ -233,19 +233,17 @@ const app=  new Vue({
         this.getEvaluationSlots()
       },
       methods:{
-        editCleaningTeam(slot){
-          {% if request.user.user_type == 'SENIORTEAMLEADER' %}
-          window.location.href='/stl/editcleaning/team/'+slot
-          {% else %}
-          window.location.href='/operation-supervisor/editcleaning/team/'+slot
-          {% endif %}"
+        editCleaningTeam(slot){          
+          window.location.href='/stl/editcleaning/team/'+slot         
         },
-        addCleaningTeam(slot){
-          {% if request.user.user_type == 'SENIORTEAMLEADER' %}
+        editCleaningTeamOpSupervisor(slot){
+          window.location.href='/operation-supervisor/editcleaning/team/'+slot  
+        },
+        addCleaningTeam(slot){          
           window.location.href='/stl/assigncleaning/team/'+slot
-          {% else %}
+        },
+        addCleaningTeamOpSupervisor(slot){
           window.location.href='/operation-supervisor/assigncleaning/team/'+slot
-          {% endif %}
         },
         selectEditSlot(slot){
          
