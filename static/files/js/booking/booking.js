22e7733ec8038eca9eb694f3c71b73149de23b0e
ADHILSHA <aadilshah56@gmail.com>
2021-07-12 19:55:10 +0530

    minor fixes on order details facade

--- static/files/js/booking/booking.js
+++ static/files/js/booking/booking.js
@@ -138,8 +138,8 @@ const app=new Vue({
   rules: {
     required: v => !!v || 'this field is required',
   },
-  url:'http://localhost:8000',
-   // url:'https://test.bleach-kw.com',
+  //url:'http://localhost:8000',
+    url:'https://test.bleach-kw.com',
     //url:'http://127.0.0.1:8000',
     kitchenData:{
         wall_type:'',
