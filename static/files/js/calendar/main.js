95655e3975f1bae546119a77d2e4167fe573ebbe
ADHILSHA <aadilshah56@gmail.com>
2021-07-20 15:36:20 +0530

    image sis=ze adjustment on order details

--- static/files/js/calendar/main.js
+++ static/files/js/calendar/main.js
@@ -17,10 +17,10 @@ $(document).ready(function(){
                 items:1
             },
             600:{
-                items:3
+                items:4
             },
             1000:{
-                items:5
+                items:6
             }
         }
     })
