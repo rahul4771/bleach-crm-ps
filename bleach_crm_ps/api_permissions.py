f8494a88a6f4ce21a28e6615eed492821eb790f8
Ansab M <ansab.m@bleach-kw.com>
2021-08-09 15:05:53 +0530

    tl permission

--- bleach_crm_ps/api_permissions.py
+++ bleach_crm_ps/api_permissions.py
@@ -4,3 +4,5 @@ class IsTeamInchargePermission(permissions.BasePermission):
     def check_perm(self, request, view):
         if request.user.user_type=='TEAMINCHARGE':
             return True
+        else:
+            return False
