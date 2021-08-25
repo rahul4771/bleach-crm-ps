424dfa86417e8f86c4075acfb962dda900ef5448
Rangeen <rangeens582@gmail.com>
2021-08-25 10:55:46 +0530

    supplier

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from inventory.models import Category,Line,Segment,Attribute,AttributeValue,InventoryItem,ItemUnit,InventoryItemImages,Bundle,BundleItems,BundleItemUnits
+from inventory.models import PurchaseOrder,PurchaseOrderItems,Category,Line,Segment,Attribute,AttributeValue,InventoryItem,ItemUnit,InventoryItemImages,Bundle,BundleItems,BundleItemUnits
 # Register your models here.
 
 admin.site.register(Category)
@@ -13,3 +13,5 @@ admin.site.register(InventoryItemImages)
 admin.site.register(Bundle)
 admin.site.register(BundleItems)
 admin.site.register(BundleItemUnits)
+admin.site.register(PurchaseOrder)
+admin.site.register(PurchaseOrderItems)
