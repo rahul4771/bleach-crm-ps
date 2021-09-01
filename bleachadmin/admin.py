36b607b5df0690af00cb7d833c7aad72fffb6fbb
Ansab M <ansab.m@bleach-kw.com>
2021-09-01 13:46:17 +0530

    reorder error solved

--- bleachadmin/admin.py
+++ bleachadmin/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from bleachadmin.models import ServiceProductivity,ServicePriceRange,Settings
+from bleachadmin.models import ServiceProductivity,ServicePriceRange,Settings,ServiceAddOns
 
 # Register your models here.
 admin.site.register(ServiceProductivity)
 admin.site.register(ServicePriceRange)
 admin.site.register(Settings)
+admin.site.register(ServiceAddOns)
--- bleachadmin/models.py
+++ bleachadmin/models.py
@@ -46,6 +46,27 @@ class ServicePriceRange(models.Model):
 	def __str__(self):
 		return self.service_type.name
 
+
+
+class ServiceAddOns(models.Model):
+	service_type     = models.ForeignKey(ServiceType,blank=True,null=True,related_name='addons_service_type')
+	name             = models.CharField(max_length=100,blank=True,null=True)
+	category         = models.CharField(max_length=100,blank=True,null=True)
+	size             = models.CharField(max_length=100,blank=True,null=True)
+	price            = models.FloatField(blank=True,null=True)
+	productivity     = models.FloatField(blank=True,null=True)
+
+	is_active    = models.BooleanField(null=False,blank=True,default=True)
+	created      = models.DateTimeField(auto_now_add=True)
+	updated      = models.DateTimeField(auto_now=True)
+	def __unicode__(self):
+		return str(self.service_type.name)
+
+	def __str__(self):
+		return self.service_type.name
+
+
+
 class Settings(models.Model):
 	discount_percentage = models.FloatField(blank=True,null=True)
 	duration            = models.IntegerField(blank=True,null=True)
