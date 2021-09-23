455876bda9094391bd4ce345ca5182b931379454
Sonu <sonu.george@bleach-kw.com>
2021-09-23 17:14:04 +0530

    inv serveice page

--- static/files/js/service.js
+++ static/files/js/service.js
@@ -2,8 +2,8 @@
 //use async funcation to call service methods
 //Check Response status always
 
-var api ='https://my.bleachkw.com';
- var url ='https://my.bleachkw.com';
+// var api ='https://my.bleachkw.com';
+//  var url ='https://my.bleachkw.com';
 
 //var api ='https://test.bleach-kw.com';
 //var url ='https://test.bleach-kw.com';
@@ -12,8 +12,8 @@ var api ='https://my.bleachkw.com';
 // var url ='http://127.0.0.1:8000';
 
 //var api='https://test.bleach-kw.com';
-// var api = 'http://localhost:8000'
-// var api = 'http://localhost:8000'
+var api = 'http://localhost:8000'
+var api = 'http://localhost:8000'
 
 
 async function _post(url,data){
