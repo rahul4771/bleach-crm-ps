cd1219f31bdc61e5c4f295f8a5a3003e793005b6
ADHIL SHA <adhilsha@ADHILs-MacBook-Pro.local>
2021-10-20 11:33:28 +0530

    calendar height isssue solved

--- static/files/js/service.js
+++ static/files/js/service.js
@@ -2,8 +2,8 @@
 //use async funcation to call service methods
 //Check Response status always
 
-//var api ='https://my.bleachkw.com';
-//var url ='https://my.bleachkw.com';
+// var api ='https://my.bleachkw.com';
+// var url ='https://my.bleachkw.com';
 
 // var api ='https://test.bleach-kw.com';
 // var url ='https://test.bleach-kw.com';
