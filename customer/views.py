fa2abda0d1260aac08204566f17799947e452da0
Ansab M <ansab.m@bleach-kw.com>
2021-10-28 15:21:08 +0530

    cleaners counts

--- customer/views.py
+++ customer/views.py
@@ -1849,35 +1849,6 @@ class GetServiceProductivity(APIView):
 			service_productivity['max_cleaners']     = serviceproductivity.max_cleaners
 			service_productivity['min_cleaners']     = serviceproductivity.min_cleaners
 
-		if service_type   == 'General Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_general_skill=True).count()		
-		elif service_type == 'Deep Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_deep_skill=True).count()
-		elif service_type == 'Upholstery Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_upholstery_skill=True).count()
-		elif service_type == 'Kitchen Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_kitchen_skill=True).count()
-		elif service_type == 'Kitchen Appliances':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_kitchen_skill=True).count()
-		elif service_type == 'Carpet Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_carpet_skill=True).count()
-		elif service_type == 'Sterilization':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_sterilization_skill=True).count()
-		elif service_type == 'Mattress Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_deep_skill=True).count()
-		elif service_type == 'Facade Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_facade_skill=True).count()
-		elif service_type == 'Storage Area':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_storagearea_skill=True).count()
-		elif service_type == 'Car Parking Umbrella':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_carparkingumbrella_skill=True).count()
-		elif service_type == 'Window Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_window_skill=True).count()
-		elif service_type == 'Outdoor Cleaning':
-			total_cleaners = UserProfile.objects.filter(Q(Q(user_type='TEAMINCHARGE')|Q(user_type='CLEANER'))).filter(is_active=True,is_outdoor_skill=True).count()	
-		else:
-			total_cleaners = 0
-
 		return JsonResponse(service_productivity)
 
 
