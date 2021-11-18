1125f3aed1ef681176dc42f070f3b620ea5c4645
Ansab M <ansab.m@bleach-kw.com>
2021-11-18 17:37:23 +0530

    changes

--- customer/views.py
+++ customer/views.py
@@ -5996,7 +5996,7 @@ class EditOrderDetails(APIView):
 
 				backup_cleaners_array = []
 				for backup_cleaner in backup_cleaners:
-					backup_cleaners_array.append(CleaningTeamMember(team=cleaning_team,member__id=backup_cleaner,start_at=backup_start_at,end_at=backup_end_at,start_time=backup_start_at.time(),end_time=backup_end_at.time(),is_backup_cleaner=True))
+					backup_cleaners_array.append(CleaningTeamMember(team=cleaning_team,member_id=backup_cleaner,start_at=backup_start_at,end_at=backup_end_at,start_time=backup_start_at.time(),end_time=backup_end_at.time(),is_backup_cleaner=True))
 				CleaningTeamMember.objects.bulk_create(backup_cleaners_array)
 
 			response_dict['success'] = True
