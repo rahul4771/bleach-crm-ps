7bce77d9c5cbac663fa81d1e7a53257e9bfbeab0
Ansab M <ansab.m@bleach-kw.com>
2022-01-05 20:46:57 +0530

    assets and

--- bleachinventory/models.py
+++ bleachinventory/models.py
@@ -29,6 +29,12 @@ MEASURING_UNIT_CHOICES=(
     ('pack','pack')
 )
 
+ITEM_TYPE_CHOICES=( 
+    ('RAW_MATERIALS','RAW_MATERIALS'), 
+    ('ASSETS','ASSETS'),
+    ('FINISHED_GOODS','FINISHED_GOODS')
+)
+
 class Category(models.Model):
     name            =   models.CharField(max_length=100,blank=False,null=False)
     category_code   =   models.CharField(max_length=50,blank=False,null=False)
@@ -82,6 +88,7 @@ class Store(models.Model):
 
 
 class InventoryItem(models.Model):
+    item_type       =   models.CharField(max_length=20,blank=False,null=False,choices=ITEM_TYPE_CHOICES)
     item_category   =   models.ForeignKey(Category,blank=False,null=False,related_name='item_category')
     item_segment    =   models.ForeignKey(Segment,blank=True,null=True,related_name='item_segment')
     item_line       =   models.ForeignKey(Line,blank=True,null=True,related_name='item_line')
