d86c72935297f221e575512361743d437f4d3eff
Ansab M <ansab.m@bleach-kw.com>
2022-01-24 15:19:05 +0530

    changes

--- accountant/admin.py
+++ accountant/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from accountant.models import PaymentHistory
 # Register your models here.
 
-admin.site.register(PaymentHistory)
+class PaymentHistoryAdmin(admin.ModelAdmin):
+	search_fields=['order__order_no']
+    
+admin.site.register(PaymentHistory,PaymentHistoryAdmin)
 
