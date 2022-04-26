e8f54084e7cada9c4d5c8cc3edd3d683a40eb56a
Ansab-123 <ansab.m@bleach-kw.com>
2022-04-26 13:43:13 +0530

    selected options

--- Api/views.py
+++ Api/views.py
@@ -2863,6 +2863,7 @@ class InvoiceSMSMailAPI(APIView):
 	def post(self,request):
 		data = False
 		
+		print(request.POST)
 		subscription_topay = request.data.get('subscription_topay',None)
 		selected_options   = request.data.get('selectedoptions')
 		options            = selected_options.split(",")
