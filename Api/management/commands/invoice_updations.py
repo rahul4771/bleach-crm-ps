f16d212970244c5b5d618b53a181e3c84e73e274
Ansab-123 <ansab.m@bleach-kw.com>
2022-05-05 12:17:58 +0530

    changes

--- Api/management/commands/invoice_updations.py
+++ Api/management/commands/invoice_updations.py
@@ -14,13 +14,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         #SUBSCRIPTION Invoices
         subscriptions = Order.objects.select_related('evaluation__customer').filter(evaluation__quatation_status='APPROVED',payment_method='SUBSCRIPTION',payment_status='PENDING',subscription_topay__gt=0)
-        print(subscriptions,"subscriptions")
+        print(subscriptions)
         print(subscriptions.count())
         #PREPAID, CLEANING BEFORE Invoices
         before_orders = Order.objects.select_related('evaluation__customer').filter(evaluation__quatation_status='APPROVED',payment_status='PENDING').filter(Q(evaluation__payment_method='PREPAID')|Q(Q(evaluation__payment_method='BREAKDOWN')&Q(preamount_paid__gt=0)))
-        print(before_orders,"before_orders")
+        print(before_orders)
         print(before_orders.count())
         #POSTPAID, CLEANING AFTER Invoices
         after_orders  = Order.objects.select_related('evaluation__customer').prefetch_related('order_scheduler_order').filter(evaluation__quatation_status='APPROVED',payment_status='PENDING').filter(Q(payment_method='POSTPAID')|Q(Q(evaluation__payment_method='BREAKDOWN')&Q(postamount_paid__gt=0))).annotate(total_cleanings_count=Count('order_scheduler_order'),completed_cleanings_count=Sum(Case(When(order_scheduler_order__work_status='CLEANING_FULFILLED',then=1),default=0,output_field=IntegerField()),remaining_cleanings_count= F('total_cleanings_count') - F('completed_cleanings_count')).filter(remaining_cleanings_count=0)
-        print(after_orders,"after_orders")
-        print(after_orders.count())
+        # print(after_orders)
+        # print(after_orders.count())
