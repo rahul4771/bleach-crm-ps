50ddfb8b347cf320d7437c7eed084efc843d3210
Ansab-123 <ansab.m@bleach-kw.com>
2022-05-21 08:39:05 -0700

    changes

--- customer/views.py
+++ customer/views.py
@@ -350,7 +350,7 @@ class Quatation(View):
 							"Description":"ONE TIME SERVICE",
 							"Quantity":"1",
 							"UnitAmount":Amount,
-							"AccountCode":1207001,
+							"AccountCode":1002,
 							"TaxType":"NONE"
 										}
 							)
