4cb7e696dc1aeace5ba00c41760daef3e6ecf9e2
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-06-01 16:14:36 +0530

    test setup

--- static/files/js/service.js
+++ static/files/js/service.js
@@ -5,8 +5,8 @@
 // var api ='https://my.bleachkw.com';
 // var url ='https://my.bleachkw.com';
 
-var api ='http://34.220.82.173:8000';
-var url ='http://34.220.82.173:8000';
+var api ='http://52.25.67.251';
+var url ='http://52.25.67.251';
 
 // var api ='http://127.0.0.1:8000';
 // var url ='http://127.0.0.1:8000';
