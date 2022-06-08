92ae335d29c8a89ae1fc9978f7b22bd441a81244
Ansab-123 <ansab.m@bleach-kw.com>
2022-06-08 06:45:34 -0700

    changes

--- Api/management/commands/bankchargeadd.py
+++ Api/management/commands/bankchargeadd.py
@@ -64,7 +64,7 @@ class Command(BaseCommand):
                                                                 data={
                                                                     "Status": "DELETED"
                                                                 } 
-                                                            )
+                                                            ).json()
                 print(delete_payment)
             break
                 
