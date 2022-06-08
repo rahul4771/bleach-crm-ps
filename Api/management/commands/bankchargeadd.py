498b8c50fa9b20641c1481c86b4c612e044cbf3a
Ansab-123 <ansab.m@bleach-kw.com>
2022-06-08 06:51:33 -0700

    deleted

--- Api/management/commands/bankchargeadd.py
+++ Api/management/commands/bankchargeadd.py
@@ -58,9 +58,10 @@ class Command(BaseCommand):
             payments = data['Payments']
             for payment in payments:
                 print(payment['PaymentID'])
+                body = {"Status":"DELETED"}
                 delete_payment = requests.post('https://api.xero.com/api.xro/2.0/Payments/'+payment['PaymentID'],
-                                                                headers=header,
-                                                                data={"Status":"DELETED",} 
+                                                                data=body,
+                                                                headers=header 
                                                             ).json()
                 print(delete_payment)
             break
