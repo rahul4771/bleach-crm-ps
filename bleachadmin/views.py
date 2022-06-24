8dd77b47b94877ffa765b22d9a5ef9eec491769e
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-06-24 20:05:39 +0530

    sales8

--- bleachadmin/views.py
+++ bleachadmin/views.py
@@ -41,21 +41,6 @@ from django.template.loader import render_to_string
 class AdminHome(IsAdmin,View):
 	def get(self,request):
 		
-		edtusers = UserProfile.objects.all()
-
-		user_mobiles = 10000000
-
-		for user in edtusers:
-			
-			user_mobiles=int(user_mobiles+1)
-			user_email = str(user_mobiles)+"@bleachkwtest.com"
-			
-			user.email = user_email
-			user.mobile_number = user_mobiles
-			user.bleach_mobile_number = user_mobiles
-			user.save()
-
-
 		#cleaners and leaders
 		cleaners = UserProfile.objects.filter(is_active=True,user_type='CLEANER')
 		leaders  = UserProfile.objects.filter(is_active=True,user_type='TEAMINCHARGE')
