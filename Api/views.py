68de45b69b89366ea11361d12d3a2886d68370c6
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-07-27 18:08:27 +0530

    assign clan task

--- Api/views.py
+++ Api/views.py
@@ -5735,10 +5735,10 @@ class SubscriptionMailAPI(APIView):
 			response_data['message'] = 'Thank you for subscribing !'
 
 			#send mail
-			# msg_html = render_to_string('email/soa.html',{"customer":customer,"address":address,"orders":accounts_list,"total_balance":total_balance,"total_credit":total_credit,"total_debit":total_debit})
-			# msg = EmailMultiAlternatives('Bleach Newsletter Subscription', '', 'notification@bleach-kw.com', [customer_email])
-			# msg.attach_alternative(msg_html, "text/html")
-			# msg.send(fail_silently=False)
+			msg_html = render_to_string('email/subscription.html')
+			msg = EmailMultiAlternatives('Bleach Newsletter Subscription', '', 'notification@bleach-kw.com', [customer_email])
+			msg.attach_alternative(msg_html, "text/html")
+			msg.send(fail_silently=False)
 		
 		return Response(response_data,HTTP_200_OK)
 
