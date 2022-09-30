5e9f914c96f279cbd43f2350d21ec9e6012a998d
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-09-30 16:45:08 +0530

    test3

--- Api/views.py
+++ Api/views.py
@@ -5951,7 +5951,7 @@ class EvaluationBookingCustomerOtpGenerationAPI(APIView):
 		
 		# sms_response = response.text
 		# message_code = sms_response[:2]
-		response_dict['liv_resp'] = re.findall(r'\d+', str(live_response))[0]
+		response_dict['liv_resp'] = re.findall(r'\d+', str(live_response)[0])[0]
 
 		# if message_code == "00":
 		response_dict['sms_status'] = "success"
