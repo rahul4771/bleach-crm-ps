adf691523ba11e3d7bb8f249dde79330b6d226cc
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-10-21 13:05:11 +0530

    test upd6

---
+++ Api/management/commands/modify_user_test.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+from django.core.management.base import BaseCommand
+
+from user.models import UserProfile
+
+from django.utils import timezone
+from datetime import timedelta,date,datetime
+from django.db.models import Q,Sum,When,Case,Value,F,Func,Count,Avg,Max,ExpressionWrapper,DateTimeField,DurationField,BigIntegerField,BooleanField,IntegerField,FloatField,CharField
+from django.db.models.functions import Cast
+from django.db.models import Prefetch
+
+class Command(BaseCommand):
+    help = 'Automatic Invoice Generations'
+
+    def handle(self, *args, **kwargs):
+        customers = UserProfile.objects.all()
+
+        first_num = 20000000
+        for customer in customers:
+            first_num = int(first_num+1)
+            print(first_num,"mob")
+            customer.mobile_number = first_num
+            customer.bleach_mobile_number = first_num
+            customer.email = str(first_num)+'@bleachtest.kw'
+            customer.save()
