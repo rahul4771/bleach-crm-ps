9bc3ac31f918f9095cd3c42a194e35901e258919
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-10-21 11:43:30 +0530

    test upd4

--- accountant/models.py
+++ accountant/models.py
@@ -22,7 +22,7 @@ class PaymentHistory(models.Model):
 	receipt_no                   = models.IntegerField(blank=True,null=True)
 	amount_paid 				 = models.FloatField(blank=True,null=True)
 	payment_mode 				 = models.CharField(max_length=100,blank=True,null=True,choices=PAYMENT_MODE_CHOICES)
-	# received_by 				 = models.ForeignKey(UserProfile,blank=True,null=True,related_name='payment_history_received_by')
+	received_by 				 = models.ForeignKey(UserProfile,blank=True,null=True,related_name='payment_history_received_by')
 	paid_date 					 = models.DateTimeField(blank=True,null=True)
 
 	check_no            		 = models.CharField(max_length=100,blank=True,null=True)
