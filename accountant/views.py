d52579283c3141c74f9549f8deb04b51ae76d917
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-10-31 19:08:36 +0530

    xero inv export

--- accountant/views.py
+++ accountant/views.py
@@ -1540,6 +1540,35 @@ def export_users_xls(request):
 	# Sheet body, remaining rows
 	font_style = xlwt.XFStyle()
 
+	if report_type == 'xeroinvoices':
+		response = HttpResponse(content_type='application/ms-excel')
+		response['Content-Disposition'] = 'attachment; filename="System xeroInvoices.xls"'
+
+		row_num = 0
+
+		font_style = xlwt.XFStyle()
+		font_style.font.bold = True
+
+		# Sheet body, remaining rows
+		font_style = xlwt.XFStyle()
+
+		wb = xlwt.Workbook(encoding='utf-8')
+		ws = wb.add_sheet('XeroInvoices')
+
+		columns = ['Date','BLC','Invoice No.','Payment Policy','Paid Amount']
+
+		for col_num in range(len(columns)):
+			ws.write(row_num, col_num, columns[col_num], font_style)
+
+		xeroinvoices = XeroInvoice.objects.filter(is_active=True,created__range=(prev_date_start,todate_date_end),is_paid=True).values_list('paid_date','order__order_no', 'payment_policy','amount').order_by('-id')
+
+		xeroinvoices = [[x.strftime("%d-%m-%Y") if isinstance(x, datetime) else x for x in row] for row in xeroinvoices ]
+
+		for row in xeroinvoices:
+			row_num += 1
+			for col_num in range(len(row)):
+				ws.write(row_num, col_num, row[col_num], font_style)
+	
 	if report_type == 'employeecommission':
 
 		response = HttpResponse(content_type='application/ms-excel')
