95fc040f7d52e40f7aa2c8fcb6c3c962a3d2dff8
rangeensuresh <rangeen.suresh@bleach-kw.com>
2022-11-22 11:22:19 +0530

    xero fix8

--- Api/management/commands/xero_bankcharge_update.py
+++ Api/management/commands/xero_bankcharge_update.py
@@ -233,7 +233,7 @@ class Command(BaseCommand):
             for invoice in invoices['Invoices']:
 
                 print(invoice['Date'])
-                print(invoice['DueDateString'].strptime('%Y-%m-%d'))
+                print(datetime(invoice['DueDateString']).strftime('%Y-%m-%d'))
                 print(invoice['DueDate'])
 
                 #Payment Removal
