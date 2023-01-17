6eece6ee469a5634c1010cff1a2b01e6932cda9a
rangeensuresh <rangeen.suresh@bleach-kw.com>
2023-01-17 17:05:58 +0530

    order in progress updates10

--- Api/management/commands/order_status_update.py
+++ Api/management/commands/order_status_update.py
@@ -108,7 +108,14 @@ class Command(BaseCommand):
         system_orders = Order.objects.filter(order_no__in=listed_orders).annotate(total_cleanings_count=Count('order_scheduler_order'), completed_cleanings_count=Sum(Case(When(order_scheduler_order__work_status='CLEANING_FULFILLED',then=1),default=0,output_field=IntegerField())), cancelled_cleanings_count=Sum(Case(When(order_scheduler_order__work_status='CLEANING_CANCELLED',then=1),default=0,output_field=IntegerField())) )
 
         count = 0
+        count2 = 0
+
         for order in system_orders:
             if order.payment_status == 'COMPLETED' and int(order.total_cleanings_count) == int(order.completed_cleanings_count)+int(order.cancelled_cleanings_count) :
+                # order.order_status = 'ORDER_CLOSED'
+                # order.save()
                 count += 1
-                print(count,order.order_no,order.total_cleanings_count,order.completed_cleanings_count,order.cancelled_cleanings_count)
+                # print(count,order.order_no,order.total_cleanings_count,order.completed_cleanings_count,order.cancelled_cleanings_count)
+            else:
+                count2 += 1
+                print(count2,order.order_no,order.total_cleanings_count,order.completed_cleanings_count,order.cancelled_cleanings_count)
