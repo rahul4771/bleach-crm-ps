3e589b959467783ed6e86843c63028d40eab7702
jishinslots <jishin.cheruvattil@slots.com.kw>
2024-05-28 13:39:59 +0530

    management command

--- Api/management/commands/automatic_item.py
+++ Api/management/commands/automatic_item.py
@@ -5,16 +5,14 @@ from django.db.models import Sum
 class Command(BaseCommand):
     def handle(self, *args, **options):
         any_item = InventoryItem.objects.all()
-        count_val = 0
         for item in any_item:
             if item.item_add_type == 'quantity':
                 store_counts = QuantityStoreDetails.objects.filter(quantity_item__name=item.name)
                 count_val = store_counts.aggregate(total_quantity=Sum('quantity'))['total_quantity'] or 0
-            old_quantity = item.total_quantity
-            item.total_quantity = count_val
-            item.save()
-            print(f"item name - {item.name} - quantity-{old_quantity} || updated - {item.total_quantity}")
+                old_quantity = item.total_quantity
 
-
-
-
+                # Only proceed if count_val is not zero and if the values are different
+                if count_val != 0 and old_quantity != count_val:
+                    item.total_quantity = count_val
+                    item.save()
+                    print(f"item name - {item.name} - old quantity: {old_quantity} || updated quantity: {item.total_quantity}")
