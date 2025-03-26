d5f266b2da9a384b7046c4bc96ed0957be8fce22
jsihin <jishin.cheruvattil@slots.com.kw>
2025-03-26 12:32:47 +0530

    invoice no generate

--- Api/management/commands/generate_sequence_invoice.py
+++ Api/management/commands/generate_sequence_invoice.py
@@ -12,15 +12,25 @@ class Command(BaseCommand):
         last_invoice_no = Order.objects.filter(
             invoice_no__isnull=False, id__lte=reference_order.id
         ).order_by('-id').first().invoice_no
-
+        updated_last_invoice_no = ""
         current_year = str(timezone.now().year)
-        new_invoice_no = (
-            str(timezone.now().year) + str(int(last_invoice_no[4:]) + 1).zfill(
-                len(last_invoice_no[4:])) if last_invoice_no and current_year == last_invoice_no[:4]
-            else current_year + '00001'
-        )
-        print("generated invoice", new_invoice_no)
         orders_to_update = Order.objects.filter(id__gt=reference_order.id).exclude(invoice_no__isnull=True).order_by(
             'id')
         for o in orders_to_update:
-            print(o.invoice_no, o.order_no)
+            if not updated_last_invoice_no:
+                new_invoice_no = (
+                    str(timezone.now().year) + str(int(last_invoice_no[4:]) + 1).zfill(
+                        len(last_invoice_no[4:])) if last_invoice_no and current_year == last_invoice_no[:4]
+                    else current_year + '00001'
+                )
+                print("if generated invoice" , new_invoice_no)
+                print("if", o.invoice_no, o.order_no)
+                updated_last_invoice_no = new_invoice_no
+            else:
+                new_invoice_no = (
+                    str(timezone.now().year) + str(int(updated_last_invoice_no[4:]) + 1).zfill(
+                        len(updated_last_invoice_no[4:])) if updated_last_invoice_no and current_year == updated_last_invoice_no[:4]
+                    else current_year + '00001'
+                )
+                print("else generated invoice" , new_invoice_no)
+                print("else", o.invoice_no, o.order_no)
