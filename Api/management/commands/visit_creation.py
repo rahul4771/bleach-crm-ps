47edc0d2f01cc59b21f9889b9d2cca47dde789c7
mahenth <mahenth.v98@gmail.com>
2025-03-27 15:31:38 +0530

    updated

--- Api/management/commands/visit_creation.py
+++ Api/management/commands/visit_creation.py
@@ -15,10 +15,10 @@ class Command(BaseCommand):
             order = Order.objects.get(evaluation__evaluation_id='BLC20250310273')
             evaluation_details = EvaluationDetails.objects.get(evaluation=evaluation)
             customer_address = Address.objects.get(id=8203)
-            order_scheduler_books = EvaluationBook.objects.filter(id__in=[17132])
+            order_scheduler_books = EvaluationBook.objects.filter(id__in=[17133])
 
             # Define schedule times
-            start_datetime = datetime.strptime('02/04/2025 8:00 AM', '%d/%m/%Y %I:%M %p')
+            start_datetime = datetime.strptime('12/04/2025 10:00 AM', '%d/%m/%Y %I:%M %p')
             end_datetime = start_datetime + timedelta(hours=10)
 
             # Create OrderScheduler entries
