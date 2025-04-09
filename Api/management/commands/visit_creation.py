e512dd23280cebd4151300a5ff1b0be08a945460
mahenth <mahenth.v98@gmail.com>
2025-04-09 13:24:53 +0530

    updated

--- Api/management/commands/visit_creation.py
+++ Api/management/commands/visit_creation.py
@@ -11,14 +11,15 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         try:
             # Fetch required objects
-            evaluation = Evaluation.objects.get(evaluation_id='BLC20250310272')
-            order = Order.objects.get(evaluation__evaluation_id='BLC20250310272')
+            evaluation = Evaluation.objects.get(evaluation_id='BLC20250410040')
+            order = Order.objects.get(evaluation__evaluation_id='BLC20250410040')
             evaluation_details = EvaluationDetails.objects.get(evaluation=evaluation)
-            customer_address = Address.objects.get(id=8198)
-            order_scheduler_books = EvaluationBook.objects.filter(id__in=[17198])
+            customer_address_id = evaluation.customer.id
+            customer_address = Address.objects.get(customer__id=customer_address_id)
+            order_scheduler_books = EvaluationBook.objects.filter(id__in=[17245])
 
             # Define schedule times
-            start_datetime = datetime.strptime('11/04/2025 10:00 AM', '%d/%m/%Y %I:%M %p')
+            start_datetime = datetime.strptime('19/04/2025 10:00 AM', '%d/%m/%Y %I:%M %p')
             end_datetime = start_datetime + timedelta(hours=10)
 
             # Create OrderScheduler entries
