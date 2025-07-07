59b5d4e363539c52adad39e32b17debf11390c0c
DEEPA B <69419333+deepab4991@users.noreply.github.com>
2025-07-07 15:32:32 +0300

    update

--- Api/admin.py
+++ Api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Api.models import XeroConnection
+from Api.models import XeroConnection, OrderScheduler # Add OrderScheduler here
 
 # Register your models here.
 admin.site.register(XeroConnection)
+admin.site.register(OrderScheduler)  # Register OrderScheduler
